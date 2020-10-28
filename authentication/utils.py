82bf6da70efd4843e9a5d5684a23681a8fdfe548
Yash Garg <yash.garg.cd.ece19@itbhu.ac.in>
2020-10-28 12:08:44 +0530

    csrf exempt

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,8 +1,16 @@
 from .models import FirebaseUser
 from firebase_admin.auth import verify_id_token
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 
 def get_firebase_user(token):
     uid = verify_id_token(token)['uid']
     # pylint: disable=no-member
     return FirebaseUser.objects.get(uid=uid)
+
+
+class CSRFExemptMixin(object):
+    @method_decorator(csrf_exempt)
+    def dipatch(self, *args, **kwargs):
+        return super(CSRFExemptMixin, self).dispatch(*args, **kwargs)
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from .serializers import NewFirebaseUserSerializer
+from .utils import CSRFExemptMixin
 
 
-class NewUserView(generics.CreateAPIView):
+class NewUserView(CSRFExemptMixin, generics.CreateAPIView):
     serializer_class = NewFirebaseUserSerializer
